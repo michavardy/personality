fb736a88bd9afd3c5553b1ce61412a7c7a9a2b8f
micha <micha.vardy@tufin.com>
2024-07-26 20:10:08 +0300

    added frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,      // Ensure this is the correct port number
+  },
+});
